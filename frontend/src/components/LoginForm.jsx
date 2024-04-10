5a419e25ac0d70ec147eafeef155da43db412fe2
Denzel <jdenzelmab@gmail.com>
2024-04-10 13:18:15 -0400

    Created LoginForm function with yup and formik library to handle user login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,5 +1,53 @@
+import { useState } from 'react'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import { useNavigate } from 'react-router-dom'
 import api from '../api'
 
+const loginSchema = Yup.object().shape({
+    username: Yup.string().required('Username Required'),
+    password: Yup.string().required('Password Required'),
+})
+
+function LoginForm({ route }) {
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <Formik initialValues={{
+            username: '',
+            password: ''
+        }}
+        validationSchema={loginSchema}
+        onSubmit={async (values, {setSubmitting}) => {
+            try {
+                const res = await api.post(route, values)
+              localStorage.setItem(ACCESS_TOKEN, res.data.access);
+              localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+              navigate("/");
+            } catch (error) {
+              alert(error);
+              setSubmitting(false)
+            } finally {
+              setLoading(false);
+            }
+        }}>
+            {({ isSubmitting }) => (
+                <Form>
+                    <h4>Username:</h4>
+                    <Field type="text" name="username"/>
+                    <ErrorMessage name="username" component="p"/> 
+                    
+                    <h4>Password:</h4>
+                    <Field type="password" name="password"/>
+                    <ErrorMessage name="password" component="p"/>
+
+                    <button type='submit' disabled={isSubmitting}>Log In</button>
+                </Form>
+            )}
+        </Formik>
+
+    )
+}
+
+export default LoginForm
