64eeee14ec09ab68181b84b561eb3399ace54d7a
Denzel <jdenzelmab@gmail.com>
2024-04-23 21:11:19 -0400

    Created ClockOutView

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,12 +12,19 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
 
 class ClockInView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = TimeClockSerializer(data = request.data)
         if serializer.is_valid:
             serializer.save()
             return Response({"message": 'Clock out successful!', "data": serializer.data}, status=status.HTTP_200_OK)
         return Response({'message': 'Clock out unsuccessful', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+class ClockOutView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    def patch(self, request, id):
+        time_clock = TimeClock.objects.get(id=id)
+        serializer = TimeClockSerializer(time_clock, data = request.data, partial=True)
 
 
 class TimeClockView(generics.ListAPIView):
