fa9cc0817ce3ead14e61e4c1ac16bb5ae7f7aaf2
Denzel <jdenzelmab@gmail.com>
2024-04-23 21:08:18 -0400

    Created ClockInView

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.models import *
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class ClockInView(generics.CreateAPIView):
+    def post(self, request):
+        serializer = TimeClockSerializer(data = request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response({"message": 'Clock out successful!', "data": serializer.data}, status=status.HTTP_200_OK)
+        return Response({'message': 'Clock out unsuccessful', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class TimeClockView(generics.ListAPIView):
     serializer_class = TimeClockSerializer
