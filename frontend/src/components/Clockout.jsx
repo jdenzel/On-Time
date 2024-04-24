dcb4716e455f7882b8d83a48b0763699d6250fe2
Denzel <jdenzelmab@gmail.com>
2024-04-23 20:53:10 -0400

    Created Clockout component

---
+++ frontend/src/components/Clockout.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import * as Yup from 'yup'
+
+
+function Clockout() {
+    
+}
+
+export default Clockout
