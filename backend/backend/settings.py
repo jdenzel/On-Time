418456ed39af482cb5cfc90b3b959889b8530ebf
Denzel <jdenzelmab@gmail.com>
2024-04-30 21:59:15 -0400

    Changed debug to True for fixing errors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY='django-insecure-tn+x#c1(zps_m!6d(!tx(uhzbj)g65)f6+)6u3q@u@to0h+vu2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
